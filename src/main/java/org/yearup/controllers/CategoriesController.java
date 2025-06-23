b2c2bc8063c71cd635ac4184c0809bdc82650d05
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-06-23 10:40:54 -0400

    Added @Autowired Constructor for CategoriesController Class

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,5 +1,6 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
@@ -20,6 +21,11 @@ public class CategoriesController
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
 
     // create an Autowired controller to inject the categoryDao and ProductDao
 
