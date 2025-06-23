409a5a8c12cf2a4b049741b4c9ad7a22f42b486d
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-06-23 10:34:27 -0400

    Added @RestController Annotation to CategoriesController Class

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -3,6 +3,7 @@ package org.yearup.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -10,10 +11,12 @@ import org.yearup.models.Product;
 
 import java.util.List;
 
-// add the annotations to make this a REST controller jacoib
+// add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+
+@RestController
 public class CategoriesController
 {
     private CategoryDao categoryDao;
