d3f7c5a7f9ab2ab899b2134e3ba857ac6dc10132
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-06-24 11:14:30 -0400

    Working on update() Method inside of MySqlCategoryDao

--- src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -118,7 +118,20 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao {
     @Override
     public void update(int categoryId, Category category)
     {
-        String 
+        String sql = """
+            UPDATE categories
+            SET name = ?,
+                description = ?
+            WHERE category_id = ?
+            """;
+        try (Connection connection = getConnection()) {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
