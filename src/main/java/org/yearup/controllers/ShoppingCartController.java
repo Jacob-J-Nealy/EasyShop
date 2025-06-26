31d2f6d018e72408a452fa2d7f27898428465fb9
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-06-26 14:38:31 -0400

    Created CartDao Interface

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -8,7 +8,9 @@ import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
 import org.yearup.models.User;
 
 import java.security.Principal;
@@ -59,7 +61,23 @@ public class ShoppingCartController
 
     // add a POST method to add a product to the cart - the url should be
     // https://localhost:8080/cart/products/15 (15 is the productId to be added
-    public void addToCart(@PathVariable int productId)
+    public void addToCart(@PathVariable int productId, @RequestBody ShoppingCartItem item, Principal principal) {
+
+        try {
+            String userName = principal.getName();
+            User user = userDao.getByUserName(userName);
+            int userId = user.getId();
+
+            Product product = productDao.getById(productId);
+            if (product == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+
+            item.setProduct(product);
+            // shoppingCartDao.add
+
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Could not add to cart");
+        }
+    }
 
 
     // add a PUT method to update an existing product in the cart - the url should be
