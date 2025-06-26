73120619efe079fa2dde1d07b0bfdbebbeba90cf
Jacob-J-Nealy <jacobnealy01@gmail.com>
2025-06-26 14:54:58 -0400

    Generated Methods and Constructor inside of MySqlCartDao Class

--- src/main/java/org/yearup/data/mysql/MySqlCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlCartDao.java
@@ -1,4 +1,34 @@
 package org.yearup.data.mysql;
 
-public class MySqlCartDao {
+import org.yearup.data.CartDao;
+import org.yearup.models.ShoppingCart;
+import org.yearup.models.ShoppingCartItem;
+
+import javax.sql.DataSource;
+
+public class MySqlCartDao extends MySqlDaoBase implements CartDao {
+
+    public MySqlCartDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+
+    }
+
+    @Override
+    public void add(int userId, ShoppingCartItem item) {
+
+    }
+
+    @Override
+    public void update(int userId, int productId, int quantity) {
+
+    }
+
+    @Override
+    public void delete(int userId) {
+
+    }
 }
